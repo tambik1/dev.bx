55f714e65aa71af11c2c11ea786a5344a0514036
Tambovskii <tambik1@gmail.com>
2021-10-17 00:30:35 +0200

    Main файл для выполнения ДЗ

---
+++ homework-2/index.php
@@ -0,0 +1,7 @@
+<?php
+/** @var array $movies */
+/** @var integer $userAge */
+/** @var integer $i */
+require "movies.php";
+require "hw-2_functions.php";
+searchMoviesByAge($movies, $userAge, $i);
